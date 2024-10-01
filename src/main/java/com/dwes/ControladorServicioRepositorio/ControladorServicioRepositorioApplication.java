49ac3f5072ff84a2b3ae8cdbdb28b01d53c2fce8
marcosar26 <marcosar26@educastur.es>
2024-10-01 19:44:05 +0200

    first commit

---
+++ src/main/java/com/dwes/ControladorServicioRepositorio/ControladorServicioRepositorioApplication.java
@@ -0,0 +1,13 @@
+package com.dwes.ControladorServicioRepositorio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControladorServicioRepositorioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControladorServicioRepositorioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dwes/ControladorServicioRepositorio/ControladorServicioRepositorioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dwes.ControladorServicioRepositorio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControladorServicioRepositorioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
