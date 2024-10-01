73116f206790dd549a7df3c1ba210d7a035ccb91
marcosar26 <marcosar26@educastur.es>
2024-10-01 19:49:36 +0200

    reformat code

--- src/main/java/com/dwes/ControladorServicioRepositorio/ControladorServicioRepositorioApplication.java
+++ src/main/java/com/dwes/ControladorServicioRepositorio/ControladorServicioRepositorioApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ControladorServicioRepositorioApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ControladorServicioRepositorioApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ControladorServicioRepositorioApplication.class, args);
+    }
 
 }
