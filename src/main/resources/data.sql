470ef3826ee3b8b4e0b0fbd57d9319f977361a56
marcosar26 <marcosar26@educastur.es>
2024-10-01 20:04:56 +0200

    add database data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,20 @@
+INSERT INTO Usuario (id, name, lastname, age)
+VALUES (1, 'John', 'Doe', 30);
+INSERT INTO Usuario (id, name, lastname, age)
+VALUES (2, 'Jane', 'Smith', 25);
+INSERT INTO Usuario (id, name, lastname, age)
+VALUES (3, 'Michael', 'Johnson', 35);
+INSERT INTO Usuario (id, name, lastname, age)
+VALUES (4, 'Emily', 'Williams', 28);
+INSERT INTO Usuario (id, name, lastname, age)
+VALUES (5, 'David', 'Brown', 40);
+INSERT INTO Usuario (id, name, lastname, age)
+VALUES (6, 'Sarah', 'Jones', 32);
+INSERT INTO Usuario (id, name, lastname, age)
+VALUES (7, 'James', 'Taylor', 27);
+INSERT INTO Usuario (id, name, lastname, age)
+VALUES (8, 'Emma', 'Martinez', 38);
+INSERT INTO Usuario (id, name, lastname, age)
+VALUES (9, 'Daniel', 'Anderson', 29);
+INSERT INTO Usuario (id, name, lastname, age)
+VALUES (10, 'Olivia', 'Garcia', 31);
