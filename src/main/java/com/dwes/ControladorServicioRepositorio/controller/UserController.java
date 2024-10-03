7e250bd67eefcc81ba2d6088c9c0577fe37f0481
chiteroman <querveros999@gmail.com>
2024-10-03 19:59:11 +0200

    Ej1: find users by lastname

--- src/main/java/com/dwes/ControladorServicioRepositorio/controller/UserController.java
+++ src/main/java/com/dwes/ControladorServicioRepositorio/controller/UserController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -24,5 +26,13 @@ public class UserController {
     public ResponseEntity<UserRequestDTO> getUserById(@PathVariable long id) {
         UserRequestDTO userRequestDTO = userService.getUserDtoById(id);
         return ResponseEntity.ok(userRequestDTO);
+    }
+
+    @GetMapping(value = "/apellido/{lastname}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<UserRequestDTO>> getUserByLastname(@PathVariable String lastname) {
+        List<UserRequestDTO> usuarios = userService.getUserDtoByLastname(lastname);
+        return usuarios.isEmpty() ?
+                ResponseEntity.notFound().build() :
+                ResponseEntity.ok(usuarios);
     }
 }
--- src/main/java/com/dwes/ControladorServicioRepositorio/service/UserService.java
+++ src/main/java/com/dwes/ControladorServicioRepositorio/service/UserService.java
@@ -6,6 +6,8 @@ import com.dwes.ControladorServicioRepositorio.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class UserService {
     private final UserRepository userRepository;
@@ -20,6 +22,20 @@ public class UserService {
         return UserRequestDTO.builder()
                 .name(userEntity.getName())
                 .lastname(userEntity.getLastname())
+                .build();
+    }
+
+    public List<UserRequestDTO> getUserDtoByLastname(String lastname) {
+        return userRepository.findByLastname(lastname)
+                .stream()
+                .map(this::mapToRequestDTO)
+                .toList();
+    }
+
+    private UserRequestDTO mapToRequestDTO(Usuario usuario) {
+        return UserRequestDTO.builder()
+                .name(usuario.getName())
+                .lastname(usuario.getLastname())
                 .build();
     }
 }
